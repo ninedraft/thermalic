964e3c3731e393b26dd123979f7714f2b440316b
ninedraft <>
2018-02-11 19:17:58 +0300

    refactor parse opml func

--- pkg/opml/opml.go
+++ pkg/opml/opml.go
@@ -1,9 +1,22 @@
 package opml
 
 import (
+	"github.com/gilliek/go-opml/opml"
 	"github.com/ninedraft/thermalic/pkg/models"
 )
 
 func ParseOPMLFile(path string) ([]models.OPMLentry, error) {
-	return nil, nil
+	OPMLdata, err := opml.NewOPMLFromFile(path)
+	if err != nil {
+		return nil, err
+	}
+	outlines := OPMLdata.Outlines()
+	feeds := make([]models.OPMLentry, 0, len(outlines))
+	for _, outline := range outlines {
+		feeds = append(feeds, models.OPMLentry{
+			Name: outline.Text,
+			URL:  outline.HTMLURL,
+		})
+	}
+	return feeds, nil
 }
