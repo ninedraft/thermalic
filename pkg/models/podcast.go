51fe9d3b81071e18cef3825f4e5fb05da85c06a3
ninedraft <>
2018-02-11 19:55:14 +0300

    refactor podcast

--- pkg/models/podcast.go
+++ pkg/models/podcast.go
@@ -8,8 +8,9 @@ type Podcast struct {
 	Name        string    `json:"name"`
 	Description string    `json:"description"`
 	EpisodesNum uint64    `json:"series_num"`
-	RSSfeed     string    `json:"url"`
+	FeedURL     string    `json:"feed_url"`
 	LastUpdated time.Time `json:"last_updated"`
 	Downloaded  uint64    `json:"downloaded"`
 	Listened    uint64    `json:"listened"`
+	Episodes    []Episode `json:"episodes"`
 }
